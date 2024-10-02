fd9a3f7bccb93af03b0693071dacc7ba4fa8f674
Afkarr1 <143926563+Afkarr1@users.noreply.github.com>
2024-10-02 22:03:22 +0700

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Konfirmasi Data</title>
+</head>
+<body>
+
+<h2>Konfirmasi Data</h2>
+
+<form action="Kontak.php" method="post">
+    <p>Nama: Muhamad Afkar Fathur Rafi</p>
+    <p>Email: Rafiafkar@upj.ac.id</p>
+    <p>Hobi: Olahraga</p>
+    <p>Jenis Kelamin: Laki-laki</p>
+    <p>Umur: 20 tahun</p>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
